dd097c639f8cd527b27cbc1c54193b64b73a4e12
Ritwik-coder <ritwik@websoftline.com>
2026-06-28 17:51:43 -0400

    Add files via upload

---
+++ activity1class7.py
@@ -0,0 +1,7 @@
+a = int(input("print give me the first number"))
+b = int(input("print give me the second number"))
+
+if a > 15 and b > 15 :
+    print ("both of these numbers are bigger than 10")
+else:
+    print  ("both of these numbers are samller than 10")
